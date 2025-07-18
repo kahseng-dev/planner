1d875431d4331006e135f9d1ea120b167c81f5aa
kahseng-dev <kahseng.chew@protonmail.com>
2025-07-18 08:12:49 +0800

    chore: update test data

--- web/src/tests/data.ts
+++ web/src/tests/data.ts
@@ -1,39 +1,48 @@
 export const data = [
     {
+        id: 1,
         title: "goal title",
         createdDateTime: new Date(),
         tasks: [{
+            id: 1,
             text: "task text",
             isCompleted: false,
         }],
     },
     {
+        id: 2,
         title: "THIS IS A LONGGGGGGGGGGGGGGGGGGGGGGGGGGG goal title 2",
         createdDateTime: new Date(),
         tasks: [
             {
+                id: 2,
                 text: "THIS IS A LONGGGGGGGGGGGGGGGGGGGGGGGGGGG TASK text",
                 isCompleted: true,
             },
             {
+                id: 3,
                 text: "this is a normal text",
                 isCompleted: false,
             },
         ],
     },
     {
+        id: 3,
         title: "Run a 5k race",
         createdDateTime: new Date(),
         tasks: [
             {
+                id: 4,
                 text: "Buy comfortable running shoes",
                 isCompleted: false,
             },
             {
+                id: 5,
                 text: "Complete a training plan",
                 isCompleted: true,
             },
             {
+                id: 6,
                 text: "Register for a local race",
                 isCompleted: true,
             },
