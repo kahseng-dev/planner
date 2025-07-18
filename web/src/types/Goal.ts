a591226c956a75981fad05845dc79b55f5809eab
kahseng-dev <kahseng.chew@protonmail.com>
2025-07-18 08:12:45 +0800

    fix: add id to goal and task types

--- web/src/types/Goal.ts
+++ web/src/types/Goal.ts
@@ -1,6 +1,7 @@
 import type { Task } from "../types/Task"
 
 export type Goal = {
+    id:number,
     title:string,
     createdDateTime:Date,
     tasks:Task[],
--- web/src/types/Task.ts
+++ web/src/types/Task.ts
@@ -1,4 +1,5 @@
 export type Task = {
+    id:number,
     text:string,
     isCompleted:boolean,
 }
