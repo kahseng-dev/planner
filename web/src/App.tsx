1aebc5a7cce3d0822edb156186e078cbb52cb8a4
kahseng-dev <kahseng.chew@protonmail.com>
2025-07-20 06:23:10 +0800

    feat: fix react router routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,26 @@
 import { useState } from "react"
-import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router"
+import { BrowserRouter, Route, Routes } from "react-router"
 
-import Login from "@components/home/login"
-import Board from "@/pages/Board"
+import Layout from "@/pages/_layout"
+import NoPage from "@/pages/_error"
+import Login from "@/pages/login"
+import Board from "@/pages/board"
+import Register from "@/pages/register"
 
 const App = () => {
   const [ isLoggedIn, setIsLoggedIn ] = useState(false)
 
   return (
-    <Router>
-      {!isLoggedIn ?
-        <div className="h-screen flex justify-center items-center bg-gray-200">
-          <Login />
-        </div>
-        :
-        <Routes>
-          <Route path="/board" element={<Board />} />
-          <Route index element={<Navigate to="/board" replace />} />
-        </Routes>
-      }
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="board" element={<Board />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
