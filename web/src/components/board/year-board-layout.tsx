b1972d89304bd20f7e42b5ea10125b1b04f5a1c8
kahseng-dev <kahseng.chew@protonmail.com>
2025-07-19 11:18:13 +0800

    feat: add year board layout

--- web/src/components/board/year-board-layout.tsx
+++ web/src/components/board/year-board-layout.tsx
@@ -1,5 +1,9 @@
-import { DateTime, Interval } from "luxon"
+import { DateTime } from "luxon"
 
+import Tag from "../../components/tag"
+import Button from "../../components/button"
+import GoalList from "../../components/goal-list"
+import Plus from "../../assets/icons/plus.svg"
 import { useHorizontalScroll } from "../../utils/useHorizontalScroll"
 
 import type { Goal } from "../../types/Goal"
@@ -12,12 +16,12 @@ const YearBoardLayout = ({ goals }:YearBoardLayoutProps) => {
 
     const today = DateTime.local()
 
-    const firstDayOfActiveMonth = today.startOf("month")
-
-    const daysOfMonth = Interval.fromDateTimes(
-        firstDayOfActiveMonth.startOf("month"), 
-        firstDayOfActiveMonth.endOf("month")
-    ).splitBy({ day: 1 }).map(day => day.start)
+    const currentYear = today.year
+    const count = 10
+    const yearsList:number[] = []
+    for (let index = currentYear; index <= (currentYear + count); index++) {
+        yearsList.push(index)
+    }
 
     const horizontalScrollRef = useHorizontalScroll()
 
@@ -25,6 +29,26 @@ const YearBoardLayout = ({ goals }:YearBoardLayoutProps) => {
         <div 
             ref={horizontalScrollRef}
             className="flex flex-row h-full overflow-x-scroll">
+            { yearsList.map(year => (
+                <div 
+                    key={year}
+                    className="group/goal p-4 min-w-1/3 border-r border-gray-200 overflow-y-scroll">
+                    <div className="flex items-center gap-2 mb-2">
+                        <p className="text-neutral-800">{year}</p>
+                        { year === currentYear && <Tag text="current" /> }
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        { goals.map(goal => {
+                            let createdDateTime = DateTime.fromJSDate(goal.createdDateTime)
+                            return year === createdDateTime.year && <GoalList key={goal.id} goal={goal} />
+                        })}
+                        <Button className="opacity-0 group-hover/goal:opacity-100 w-full flex items-center gap-2">
+                            <img className="size-4" src={Plus} alt="plus" />
+                            Add goal
+                        </Button>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
