b0bdd5fe7e4a15080470ef05a1a8fd6e883b4bde
kahseng-dev <kahseng.chew@protonmail.com>
2025-07-19 09:03:55 +0800

    chore: update test data

--- web/src/tests/data.ts
+++ web/src/tests/data.ts
@@ -2,49 +2,75 @@ export const data = [
     {
         id: 1,
         title: "goal title",
-        createdDateTime: new Date(),
-        tasks: [{
-            id: 1,
-            text: "task text",
-            isCompleted: false,
-        }],
+        createdDateTime: new Date('2025-07-18T00:00:00'),
+        tasks: [
+            {
+                id: 1,
+                text: "task text",
+                isCompleted: false,
+            }
+        ],
     },
     {
         id: 2,
+        title: "another goal",
+        createdDateTime: new Date('2025-07-18T00:00:00'),
+        tasks: [
+            {
+                id: 2,
+                text: "task",
+                isCompleted: false,
+            }
+        ],
+    },
+    {
+        id: 3,
         title: "THIS IS A LONGGGGGGGGGGGGGGGGGGGGGGGGGGG goal title 2",
         createdDateTime: new Date('2025-07-10T00:00:00'),
         tasks: [
             {
-                id: 2,
+                id: 3,
                 text: "THIS IS A LONGGGGGGGGGGGGGGGGGGGGGGGGGGG TASK text",
                 isCompleted: true,
             },
             {
-                id: 3,
+                id: 4,
                 text: "this is a normal text",
                 isCompleted: false,
             },
         ],
     },
     {
-        id: 3,
+        id: 4,
         title: "Run a 5k race",
         createdDateTime: new Date('2025-07-14T00:00:00'),
         tasks: [
             {
-                id: 4,
+                id: 5,
                 text: "Buy comfortable running shoes",
                 isCompleted: false,
             },
             {
-                id: 5,
+                id: 6,
                 text: "Complete a training plan",
                 isCompleted: true,
             },
             {
-                id: 6,
+                id: 7,
                 text: "Register for a local race",
                 isCompleted: true,
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: "today goal",
+        createdDateTime: new Date(),
+        tasks: [
+            {
+                id: 8,
+                text: "Buy food",
+                isCompleted: false,
             },
         ],
     }
