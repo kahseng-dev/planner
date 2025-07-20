4c1ece921610edb36ed637d43c5692b3852d9261
kahseng-dev <kahseng.chew@protonmail.com>
2025-07-21 03:09:31 +0800

    feat: add axios fetch api service

---
+++ web/src/services/api.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const SERVER_PORT = 8081
+const API_URL = `http://localhost:${SERVER_PORT}/api`
+
+axios.defaults.baseURL = API_URL
+axios.defaults.headers.post['Content-Type'] = "application/json"
+
+export const getAuthToken = () => {
+  return window.localStorage.getItem("auth_token")
+}
+
+export const setAuthToken = (token:string) => {
+  return window.localStorage.setItem("auth_token", token)
+}
+
+export const request = (method:string, url:string, data:any) => {
+  let token = getAuthToken()
+  let headers = {}
+
+  if (token) {
+    headers = {"Authorization" : `Bearer ${token}`}
+  }
+
+  return axios({
+    method: method, 
+    headers: headers,
+    url: url,
+    data: data,
+  })
+}
+
+export const login = async (email:string, password:string) => {
+  request(
+    "POST", 
+    "/api/users/login", 
+    { email: email, password: password })
+    .then(response => setAuthToken(response.data.token))
+    .catch(error => console.error(error))
+}
+
+export const register = async (name:string, email:string, password:string) => {
+  request(
+    "POST", 
+    "/api/users/register", 
+    { name: name, email: email, password: password, })
+    .then(response => setAuthToken(response.data.token))
+    .catch(error => console.error(error))
+}
